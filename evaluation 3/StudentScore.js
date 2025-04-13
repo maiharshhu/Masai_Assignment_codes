d24c11d27816e709388a0b6e5d420b78461938f4
Maiharshhu <hj.harsh440@gmail.com>
2025-04-13 23:33:09 +0530

    StudentScores

---
+++ evaluation 3/StudentScore.js
@@ -0,0 +1,24 @@
+// Array of student scores
+let studentScores = [35, 67, 84, 56, 72, 97, 48, 65, 89, 38];
+let length = studentScores.length;
+let passCount = 0;
+
+// Update scores based on conditions
+for (let i = 0; i < length; i++) {
+    if (studentScores[i] < 40) {
+        studentScores[i] += 20;
+    } else if (studentScores[i] > 90) {
+        studentScores[i] = 90;
+    }
+}
+
+// Count passing students
+for (let i = 0; i < length; i++) {
+    if (studentScores[i] >= 50) {
+        passCount++;
+    }
+}
+
+// Print results
+console.log("Number of passing students:", passCount);
+console.log("Updated Scores:", studentScores);
