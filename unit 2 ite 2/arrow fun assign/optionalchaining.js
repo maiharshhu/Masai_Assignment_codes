7d83d6f7263fe16865e3237848a698914956ca81
Maiharshhu <hj.harsh440@gmail.com>
2025-06-22 23:20:14 +0530

    .

--- unit 2 ite 2/arrow fun assign/optionalchaining.js
+++ unit 2 ite 2/arrow fun assign/optionalchaining.js
@@ -1,6 +1,8 @@
 const user = { id: 123, profile: { name: "John Doe", address: { city: "Los Angeles", zipcode: "90001" } } };
 
-let {id,profile:{name, address:{city,zipcode}}} = user
+// let {id, profile:{name, address:{city,zipcode}}} = user
+
+let { id, profile, profile: { address } } = user;
 
 console.log(`User ${profile.name} (ID:${id}) lives in ${address?.city ?? "Information not available"} 
     (ZIP: ${address?.zipcode ?? "Information not avaliable"}) `)
