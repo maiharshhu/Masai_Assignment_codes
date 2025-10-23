d8d37ce59a269a1f974bdd0f0528cfc8f887c8a8
Maiharshhu <hj.harsh440@gmail.com>
2025-10-23 23:08:57 +0530

    Redux

---
+++ unit 4/intro to redux/ReactWithRedux/src/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './counterSlice';
+
+const Counter = () => {
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <h2>Counter: {count}</h2>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
+            <p>{JSON.stringify({ count })}</p>
+        </div>
+    );
+};
+
+export default Counter;
