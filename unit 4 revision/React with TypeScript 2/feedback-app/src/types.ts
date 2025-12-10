fa6a058f9dbe0a22cb10c8bdb9b0d0a62aae084d
Maiharshhu <hj.harsh440@gmail.com>
2025-12-10 23:18:31 +0530

    typescript with react 2

---
+++ unit 4 revision/React with TypeScript 2/feedback-app/src/types.ts
@@ -0,0 +1,7 @@
+
+export interface FeedbackData {
+  name: string;      // Customer name
+  email: string;     // Customer email
+  rating: number;    // Rating score
+  feedback: string;  // Feedback comments
+}
