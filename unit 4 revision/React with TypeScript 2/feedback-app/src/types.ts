0d0837a18401c7de671a5573fbf4691884f81668
Maiharshhu <hj.harsh440@gmail.com>
2025-12-10 23:43:34 +0530

    minor

--- unit 4 revision/React with TypeScript 2/feedback-app/src/types.ts
+++
@@ -1,7 +0,0 @@
-
-export interface FeedbackData {
-  name: string;      // Customer name
-  email: string;     // Customer email
-  rating: number;    // Rating score
-  feedback: string;  // Feedback comments
-}
